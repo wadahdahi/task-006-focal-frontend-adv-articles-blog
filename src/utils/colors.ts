786ae2ea68fe4d72b10529da926270abcbb1500a
Wadah Dahi <waddahdahi22@gmail.com>
2025-12-14 02:55:06 -0800

    fix: correct image extensions in JSON

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -11,14 +11,6 @@ export const colorPairs: ColorPair[] = [
   { bg: "bg-[#EEF4FF]", text: "text-[#3538CD]" },
   { bg: "bg-[#F0F9FF]", text: "text-[#026AA2]" },
 ];
-// TAILWIND CSS COLORS REFERENCE
-// export const colorPairs: ColorPair[] = [
-//   { bg: "bg-green-200", text: "text-green-800" },
-//   { bg: "bg-blue-200", text: "text-blue-800" },
-//   { bg: "bg-purple-200", text: "text-purple-800" },
-//   { bg: "bg-yellow-200", text: "text-yellow-800" },
-//   { bg: "bg-pink-200", text: "text-pink-800" },
-// ];
 
 // GETTING A RANDOM COLOR PAIR
 export const getRandomColorPair = (): ColorPair => {
